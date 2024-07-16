d5183750db79ad4272d003b64d70a8f8b000a838
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-07-16 03:16:00 +0300

    .

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.post('/transcribe', upload.single('audio'), (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server iss running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
