ca2747786e03391a73bac4fc2b7068dfc13d4d9b
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-07-19 00:00:03 +0300

    return tiny

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 app = Flask(__name__)
 
 # Set up Whisper model and output folder
-model = whisper.load_model("medium")
+model = whisper.load_model("tiny")
 app.config['UPLOAD_FOLDER'] = 'temp'  # Define your upload folder
 output_folder = "transcriptions"  # Adjust this path as per your setup
 
