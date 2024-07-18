86c8260eecb998760fc48a0674d23df3832aaf2c
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-07-18 22:53:50 +0300

    tiny

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 app = Flask(__name__)
 
 # Set up Whisper model and output folder
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 output_folder = "transcriptions"  # Adjust this path as per your setup
 
 @app.route('/transcribe', methods=['POST'])
